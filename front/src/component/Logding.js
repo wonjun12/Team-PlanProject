9edd0e5130a9733be63fe3a4887b7473aa0e112b
ksojin1 <trueksj1121@naver.com>
2022-11-24 02:33:28 +0900

    숙소 삭제 진행중

--- front/src/component/Logding.js
+++ front/src/component/Logding.js
@@ -6,6 +6,7 @@ import Styles from "../route/SetPlan.module.scss";
 const Hotel = () => {
 
   const { viewCont, setViewCont } = useContext(ThemeContext);
+  const [open, setOpen] = useState(0);
   const [logding, setLogding] = useState({
     0: {
       address: "",
@@ -39,6 +40,17 @@ const Hotel = () => {
         memo: "",
       }
     });
+    setOpen(idxKey);
+  }
+
+  ///삭제 진행중
+  const deleteFnc = (idx) => {
+    if(idx > 0){
+      const currentObj = Object.assign({}, logding);
+      delete currentObj[idx];
+      setLogding(currentObj);
+      setOpen(idx-1);
+    }
   }
 
   const hotelPostFnc = () => {
@@ -47,56 +59,63 @@ const Hotel = () => {
     //setViewCont(0);
   }
 
+  console.log(logding)
+
   return (
     <div className={Styles.logdingWrap}>
-      <p>숙소 설정</p>
       {Object.entries(logding).map(([idx, obj]) => (
         <div className={Styles.logdingDiv} key={idx}>
-          <label htmlFor={`address${idx}`}>
-            주소<br/>
-            <input type="text" id={`address${idx}`}
-              value={obj.address}
-              onChange={(e) => valueChangeFnc(e, "address", idx, obj)}
-            />
-          </label>
-          <label>
-            숙박일정
-            <div className={Styles.logDateDiv}>
-              <input type="date"
-                value={obj.check_in}
-                onChange={(e) => valueChangeFnc(e, "check_in", idx, obj)}
-              />
-              <p className={Styles.dateText}>~</p>
-              <input type="date"
-                value={obj.check_out}
-                onChange={(e) => valueChangeFnc(e, "check_out", idx, obj)}
-              />
-            </div>
-          </label>
-          
-          <label htmlFor={`reservation${idx}`}>
-            <input type="checkbox" id={`reservation${idx}`} checked={obj.reservation}
-              onChange={(e) => valueChangeFnc(e, "reservation", idx, obj)}
-            />
-            예약완료
-          </label>
 
-          <label htmlFor={`price${idx}`}>
-            숙소가격<br/>
-            <input type="number" id={`price${idx}`}
-            value={obj.price}
-            onChange={(e) => valueChangeFnc(e, "price", idx, obj)}
-            />
-          </label>
-          
-          <label htmlFor={`memo${idx}`}>
-            메모<br/>
-            <textarea id={`memo${idx}`} rows="5"
-              value={obj.memo}
-              onChange={(e) => valueChangeFnc(e, "memo", idx, obj)}
-            ></textarea>
-          </label>
-          
+          <div className={Styles.openDiv}>
+            <p>{parseInt(idx) + 1}번째 숙소</p>
+            <span className={Styles.openSpan} onClick={() => setOpen(parseInt(idx))}>▼</span>
+          </div>
+          {(open === parseInt(idx)) &&
+            <>
+              <label htmlFor={`address${idx}`}>
+                주소<br />
+                <input type="text" id={`address${idx}`}
+                  value={obj?.address}
+                  onChange={(e) => valueChangeFnc(e, "address", idx, obj)}
+                />
+              </label>
+              <label>
+                숙박일정
+                <div className={Styles.logDateDiv}>
+                  <input type="date"
+                    value={obj?.check_in}
+                    onChange={(e) => valueChangeFnc(e, "check_in", idx, obj)}
+                  />
+                  <p className={Styles.dateText}>~</p>
+                  <input type="date"
+                    value={obj?.check_out}
+                    onChange={(e) => valueChangeFnc(e, "check_out", idx, obj)}
+                  />
+                </div>
+              </label>
+
+              <div className={Styles.logdingInfoDiv}>
+                <input type="checkbox" id={`reservation${idx}`} checked={obj.reservation}
+                  onChange={(e) => valueChangeFnc(e, "reservation", idx, obj)} />
+                <label className={Styles.reservLabel}
+                  htmlFor={`reservation${idx}`}>예약완료</label>
+
+                <label htmlFor={`price${idx}`} className={Styles.priceLabel}>숙소가격</label>
+                <input type="number" id={`price${idx}`}
+                  value={obj?.price}
+                  onChange={(e) => valueChangeFnc(e, "price", idx, obj)} />
+              </div>
+
+              <label htmlFor={`memo${idx}`}>
+                메모<br />
+                <textarea id={`memo${idx}`} rows="5"
+                  value={obj?.memo}
+                  onChange={(e) => valueChangeFnc(e, "memo", idx, obj)}
+                ></textarea>
+              </label>
+              <input className={Styles.deleteBtn} type="button" value="삭제" onClick={() => deleteFnc(idx)}/>
+            </>
+          }
         </div>
       ))}
       <input type="button" value="숙소추가" onClick={hotelAddFnc} />
